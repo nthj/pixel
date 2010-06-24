5eb3516f1de503c7813cb021c3391da14f3e06c9
Nathaniel Jones <nathaniel@pointeractive.com>
2010-06-24 10:10:54 -0500

    Return newly calculated dimensions if given original dimensions

--- lib/pixel/pixel.rb
+++ lib/pixel/pixel.rb
@@ -1,9 +1,14 @@
 class Pixel
-  def calculate
-    [@number, @number]
+  def calculate dimensions = nil
+    [@number, @wide || ( @number.to_f / dimensions.first.to_f * dimensions.last.to_f).to_i ]
+  end
+  
+  def high
+    @wide = nil
+    self
   end
   
   def initialize number
-    @number = number
+    @number = @wide = number
   end
 end
--- spec/pixel_spec.rb
+++ spec/pixel_spec.rb
@@ -10,4 +10,10 @@ describe Pixel do
   it "should return the specified pixels as a dimensions array" do
     Pixel.new(50).calculate.should eql [50, 50]
   end
+  
+  it "should calculate the new width if we fix height" do
+    { [100, 200] => [50, 100], [82, 620] => [50, 378], [729, 65] => [50, 4] }.each do |original, calculated|
+      Pixel.new(50).high.calculate(original).should eql calculated
+    end
+  end
 end
